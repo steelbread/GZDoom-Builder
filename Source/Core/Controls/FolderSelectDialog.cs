d35a312a79d544eec67975352094750616face77
steelbread <steelbread@hotmail.com>
2018-07-30 21:40:24 -0400

    Fixed Build for my environment.

---
+++ Source/Core/Controls/FolderSelectDialog.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeImp.DoomBuilder.Controls
+{
+
+    internal class FolderSelectDialog
+    {
+        public string Title;
+        public string InitialDirectory;
+        public string FileName;
+        
+        public bool ShowDialog(System.IntPtr Handle)
+        {
+
+            return true;
+        }
+
+
+    }
+
+
+}
